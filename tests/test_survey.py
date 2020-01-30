655e7708428f95a751955c271a0fe8902fec6162
Michał Jabłoński <amm.jablonscy@gmail.com>
2020-01-30 15:48:06 +0100

    added a test for importing numeric question from surveyjs

--- tests/test_survey.py
+++ tests/test_survey.py
@@ -1,3 +1,31 @@
 # pylint:disable=missing-docstring,redefined-outer-name
+from survey_toolkit.core import Survey, NumericInputQuestion
 
-# TODO: prohibit adding a survey question that's already there
+def test_from_surveyjs_parses_numeric_question():
+    survey_json = {
+        "pages": [
+            {
+                "name": "page1",
+                "elements": [
+                    {
+                        "type": "text",
+                        "name": "age",
+                        "title": "How old are you?",
+                        "inputType": "number"
+                    }
+                ]
+            }
+        ]
+    }
+    survey_results = [
+        '{"age": 20}',
+        '{"age": 30}',
+        '{"age": 35.5}',
+        '{}',
+    ]
+    survey = Survey.from_surveyjs(survey_json, survey_results)
+    question = survey.questions[0]
+    assert type(question) == NumericInputQuestion
+    assert question.name == 'age'
+    assert question.label == 'How old are you?'
+    assert question.answers == [20, 30, 35.5, None]
