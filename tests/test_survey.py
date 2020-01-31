263cc640a55d269aba1bae091dfe1e01d6cb8705
Michał Jabłoński <amm.jablonscy@gmail.com>
2020-01-31 21:12:30 +0100

    refactored tests for Survey class

--- tests/test_survey.py
+++ tests/test_survey.py
@@ -1,31 +1,26 @@
 # pylint:disable=missing-docstring,redefined-outer-name
+import pytest
 from survey_toolkit.core import (Survey, NumericInputQuestion, TextInputQuestion,
                                  SingleChoiceQuestion)
 
 
-def test_from_surveyjs_parses_numeric_question():
-    survey_json = {
-        "pages": [
-            {
-                "name": "page1",
-                "elements": [
-                    {
-                        "type": "text",
-                        "name": "age",
-                        "title": "How old are you?",
-                        "inputType": "number"
-                    }
-                ]
-            }
-        ]
-    }
-    survey_results = [
-        '{"age": 20}',
-        '{"age": 30}',
-        '{"age": 35.5}',
-        '{}',
-    ]
-    survey = Survey.from_surveyjs(survey_json, survey_results)
+@pytest.fixture()
+def basic_surveyjs_json():
+    return {"pages": [{"name": "page1", "elements": []}]}
+
+
+def _get_surveyjs_json(basic_surveyjs_json: dict, question: dict):
+    basic_surveyjs_json["pages"][0]["elements"].append(question)
+    return basic_surveyjs_json
+
+
+def test_from_surveyjs_parses_numeric_question(basic_surveyjs_json):
+    survey_json = _get_surveyjs_json(
+        basic_surveyjs_json,
+        {"type": "text", "name": "age", "title": "How old are you?", "inputType": "number"}
+    )
+    survey_result = ['{"age": 20}', '{"age": 30}', '{"age": 35.5}', '{}']
+    survey = Survey.from_surveyjs(survey_json, survey_result)
     question = survey.questions[0]
     assert type(question) == NumericInputQuestion
     assert question.name == 'age'
@@ -33,21 +28,12 @@ def test_from_surveyjs_parses_numeric_question():
     assert question.answers == [20, 30, 35.5, None]
 
 
-def test_from_surveyjs_parses_text_question():
-    survey_json = {
-        "pages": [
-            {
-                "name": "page1",
-                "elements": [
-                    {
-                        "type": "text",
-                        "name": "surveyjsOpinion",
-                        "title": "How do you like the surveyjs service?",
-                    }
-                ]
-            }
-        ]
-    }
+def test_from_surveyjs_parses_text_question(basic_surveyjs_json):
+    survey_json = _get_surveyjs_json(
+        basic_surveyjs_json,
+        {"type": "text", "name": "surveyjsOpinion",
+         "title": "How do you like the surveyjs service?"}
+    )
     survey_results = [
         '{"surveyjsOpinion": "it\'s fantastic!"}',
         '{"surveyjsOpinion": "sux!"}',
@@ -62,35 +48,11 @@ def test_from_surveyjs_parses_text_question():
     assert question.answers == ["it's fantastic!", "sux!", "", None]
 
 
-def test_from_surveyjs_parses_text_question_with_numeric_validator():
-    survey_json = {
-        "pages": [
-            {
-                "name": "page1",
-                "elements": [
-                    {
-                        "type": "text",
-                        "name": "age",
-                        "title": "How old are you?",
-                        "validators": [
-                            {
-                                "type": "numeric",
-                                "minValue": 5,
-                                "maxValue": 105
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
-    survey_results = [
-        '{"age": "20"}',
-        '{"age": "30"}',
-        '{"age": "35.5"}',
-        '{"age": "35,5"}',
-        '{}',
-    ]
+def test_from_surveyjs_parses_text_question_with_numeric_validator(basic_surveyjs_json):
+    question_json  = {"type": "text", "name": "age", "title": "How old are you?",
+                      "validators": [{"type": "numeric", "minValue": 5, "maxValue": 105 }]}
+    survey_json = _get_surveyjs_json(basic_surveyjs_json, question_json)
+    survey_results = ['{"age": "20"}', '{"age": "30"}', '{"age": "35.5"}', '{"age": "35,5"}', '{}']
     survey = Survey.from_surveyjs(survey_json, survey_results)
     question = survey.questions[0]
     assert type(question) == NumericInputQuestion
@@ -99,38 +61,16 @@ def test_from_surveyjs_parses_text_question_with_numeric_validator():
     assert question.answers == [20, 30, 35.5, 35.5, None]
 
 
-def test_from_surveyjs_parses_radiogroup_question_when_choices_given_as_kv():
-    survey_json = {
-        "pages": [
-            {
-                "name": "page1",
-                "elements": [
-                    {
-                        "type": "radiogroup",
-                        "name": "gender",
-                        "title": "What's your gender?",
-                        "hasOther": True,
-                        "choices": [
-                            {
-                                "value": "male",
-                                "text": "Male"
-                            },
-                            {
-                                "value": "female",
-                                "text": "Female"
-                            }
-                        ]
-                    }
-                ]
-            }
+def test_from_surveyjs_parses_radiogroup_question_when_choices_given_as_kv(basic_surveyjs_json):
+    question_json = {
+        "type": "radiogroup", "name": "gender", "title": "What's your gender?", "hasOther": True,
+        "choices": [
+            {"value": "male", "text": "Male"},
+            {"value": "female", "text": "Female"},
         ]
     }
-    survey_results = [
-        '{"gender": "male"}',
-        '{"gender": "female"}',
-        '{"gender": "other"}',
-        '{}',
-    ]
+    survey_json = _get_surveyjs_json(basic_surveyjs_json, question_json)
+    survey_results = ['{"gender": "male"}', '{"gender": "female"}', '{"gender": "other"}', '{}']
     survey = Survey.from_surveyjs(survey_json, survey_results)
     question = survey.questions[0]
     assert type(question) == SingleChoiceQuestion
